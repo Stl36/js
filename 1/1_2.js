21da89391ad1b1390e950ee6072d718e2b4d231d
Aleksey Shmykov <shmykov.job@gmail.com>
2021-09-18 18:00:42 +0300

    some update

--- 1/1_2.js
+++
@@ -1,9 +0,0 @@
-/*
-2. Объявить две переменные: admin и name. Записать в name строку "Василий". Скопировать значение из name в admin. Вывести в консоль переменную admin (должно вывести "Василий").
-*/
-"use strict;"
-let admin = null;
-let myname = null;
-myname = "Василий";
-admin = myname;
-console.log(admin);
