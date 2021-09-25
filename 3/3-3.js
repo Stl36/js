873871ec24626348927717e98cd64f21c7ee6df1
Aleksey Shmykov <shmykov.job@gmail.com>
2021-09-25 09:39:24 +0300

    Update 3-3.js

--- 3/3-3.js
+++ 3/3-3.js
@@ -4,3 +4,23 @@
 */
 
 
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+products.forEach(function (element) {
+    let tempVar = element["price"]
+    element["price"] = tempVar * 0.85
+    console.log(element["price"])
+})
